a712ac7b3adb7a6609e37d29d81969c80660b133
Ta Quang Duy <danh7598@gmail.com>
2017-12-04 20:26:52 +0700

    Fix loi

--- app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
+++ app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.tqd.duy.adapter.TableAdapter;
 import com.tqd.duy.models.Table;
 
 import java.util.ArrayList;
@@ -26,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private void addControls() {
         lvTable = findViewById(R.id.lvTable);
         dsTable = new ArrayList<>();
-        dsTable.add(new Table(1,"Trống"));
-        dsTable.add(new Table(2,"Đang dọn dẹp"));
-        dsTable.add(new Table(3,"Trống"));
-        dsTable.add(new Table(4,"Có khách"));
-        dsTable.add(new Table(5,"Trống"));
-        adapterTable = new ArrayAdapter<Table>(MainActivity.this,R.layout.itemtable,dsTable);
+        dsTable.add(new Table("Bàn số 1","Trống"));
+        dsTable.add(new Table("Bàn số 2","Đang dọn dẹp"));
+        dsTable.add(new Table("Bàn số 3","Trống"));
+        dsTable.add(new Table("Bàn số 4","Có khách"));
+        dsTable.add(new Table("Bàn số 5","Trống"));
+        adapterTable = new TableAdapter(MainActivity.this,R.layout.itemtable,dsTable);
         lvTable.setAdapter(adapterTable);
 
     }
