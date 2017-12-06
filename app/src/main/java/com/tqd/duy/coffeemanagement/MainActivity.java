532fc511a888ad002f518fb24c4b6459b38f62f2
Ta Quang Duy <danh7598@gmail.com>
2017-12-06 19:06:46 +0700

    Thay doi giao dien

--- app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
+++ app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
@@ -3,18 +3,20 @@ package com.tqd.duy.coffeemanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import com.tqd.duy.adapter.TableAdapter;
+import com.tqd.duy.adapter.TableAdapter2;
 import com.tqd.duy.models.Table;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lvTable;
+    GridView gvTable;
     ArrayList<Table> dsTable;
-    ArrayAdapter<Table> adapterTable;
+    TableAdapter2 adapterTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +27,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addControls() {
-        lvTable = findViewById(R.id.lvTable);
+        gvTable = findViewById(R.id.gvTable);
         dsTable = new ArrayList<>();
         dsTable.add(new Table("Bàn số 1","Trống"));
         dsTable.add(new Table("Bàn số 2","Đang dọn dẹp"));
         dsTable.add(new Table("Bàn số 3","Trống"));
         dsTable.add(new Table("Bàn số 4","Có khách"));
-        dsTable.add(new Table("Bàn số 5","Trống"));
-        adapterTable = new TableAdapter(MainActivity.this,R.layout.itemtable,dsTable);
-        lvTable.setAdapter(adapterTable);
+        dsTable.add(new Table("Bàn số 5","Có đặt trước"));
+        dsTable.add(new Table("Bàn số 6","Trống"));
+        adapterTable = new TableAdapter2(MainActivity.this,R.layout.itemtablegird,dsTable);
+        gvTable.setAdapter(adapterTable);
 
     }
 
