faa1f49fb29392674e8ac9d51fbcc3b3017441d5
Ta Quang Duy <danh7598@gmail.com>
2017-12-09 21:44:24 +0700

    Set code example

--- app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
+++ app/src/main/java/com/tqd/duy/coffeemanagement/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
 import com.tqd.duy.adapter.TableAdapter;
+import com.tqd.duy.models.Example;
 import com.tqd.duy.models.Table;
 import java.util.ArrayList;
 //cái import nào không dùng thì xóa đi
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private void addControls() {
         gvTable = findViewById(R.id.activity_main_gv_table);
         dsTable = new ArrayList<>();
+        Example example1 = new Example(MainActivity.this,dsTable);
+        example1.SetTableExample();
         adapterTable = new TableAdapter(MainActivity.this,R.layout.item_table,dsTable);
         gvTable.setAdapter(adapterTable);
         //viết hàm tạo dữ liệu giả ở models rồi gọi ra nhé, không viết ở đây
