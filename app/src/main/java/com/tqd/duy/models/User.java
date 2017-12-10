420dec14f0bbade5d6ffca4927310a3dbd59c275
Ta Quang Duy <danh7598@gmail.com>
2017-12-10 14:33:27 +0700

    Create login screen

---
+++ app/src/main/java/com/tqd/duy/models/User.java
@@ -0,0 +1,34 @@
+package com.tqd.duy.models;
+
+/**
+ * Created by Danh on 12/10/2017.
+ */
+
+public class User {
+    private String userName;
+    private String userPassword;
+
+    public User(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
---
+++ app/src/main/java/com/tqd/duy/models/UserExample.java
@@ -0,0 +1,12 @@
+package com.tqd.duy.models;
+
+import android.app.Activity;
+
+/**
+ * Created by Danh on 12/10/2017.
+ */
+
+public class UserExample {
+
+
+}
