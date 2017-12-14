72dcfd7763d29de9fc003e6dd23127e5f3e075a1
Ta Quang Duy <danh7598@gmail.com>
2017-12-14 15:11:22 +0700

    Display login screen

--- app/src/main/java/com/tqd/duy/coffeemanagement/LoginScreen.java
+++ app/src/main/java/com/tqd/duy/coffeemanagement/LoginScreen.java
@@ -25,7 +25,7 @@ public class LoginScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen);
         addControls();
         addEvents();
     }
