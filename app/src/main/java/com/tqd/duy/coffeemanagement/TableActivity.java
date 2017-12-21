ccb484d3283cd79519f746fd95175890b853121c
Ta Quang Duy <danh7598@gmail.com>
2017-12-22 00:11:03 +0700

    Create float action button

--- app/src/main/java/com/tqd/duy/coffeemanagement/TableActivity.java
+++ app/src/main/java/com/tqd/duy/coffeemanagement/TableActivity.java
@@ -1,8 +1,12 @@
 package com.tqd.duy.coffeemanagement;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.Toast;
+
 import com.tqd.duy.adapter.TableAdapter;
 import com.tqd.duy.models.Table;
 import java.util.ArrayList;
@@ -11,6 +15,7 @@ public class TableActivity extends AppCompatActivity {
     private GridView gvTable;
     private ArrayList<Table> listTable;
     private TableAdapter adapterTable;
+    private FloatingActionButton floatBtnSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +24,8 @@ public class TableActivity extends AppCompatActivity {
         addEvents();
     }
     private void addControls() {
-        gvTable = findViewById(R.id.activity_main_gv_table);
+        gvTable = findViewById(R.id.activity_table_gv_table);
+        floatBtnSetting = findViewById(R.id.activity_table_float_btn_setting);
         listTable = new ArrayList<>();
         Table table = new Table();
         listTable = table.getSampleTable(this);
@@ -27,5 +33,14 @@ public class TableActivity extends AppCompatActivity {
         gvTable.setAdapter(adapterTable);
     }
     private void addEvents() {
+        floatBtnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processSettingActivity();
+            }
+        });
+    }
+    private void processSettingActivity() {
+        Toast.makeText(this, getString(R.string.Setting), Toast.LENGTH_SHORT).show();
     }
 }
